af7c0ff112fde0bc5b67a82acddc39a3490a96e6
arthurqui <arthur.quintanilha@hotmail.com>
2022-01-03 16:29:32 -0300

    finalização do LDABDK, o bot que ve a lista de descobrimento da steam sozinho

---
+++ LDABDK/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿            
+namespace botmap
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_iniciar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_iniciar
+            // 
+            this.btn_iniciar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(137)))), ((int)(((byte)(84)))));
+            this.btn_iniciar.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_iniciar.Location = new System.Drawing.Point(220, 230);
+            this.btn_iniciar.Name = "btn_iniciar";
+            this.btn_iniciar.Size = new System.Drawing.Size(146, 63);
+            this.btn_iniciar.TabIndex = 0;
+            this.btn_iniciar.Text = "Iniciar";
+            this.btn_iniciar.UseVisualStyleBackColor = false;
+            this.btn_iniciar.Click += new System.EventHandler(this.btn_iniciar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(186, 318);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(220, 23);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "youtube.com/brasildropkeys";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(198, 175);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 23);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Feito por ``Paraiba BDK``";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(587, 373);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_iniciar);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "LDA -BDK";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_iniciar;
+        private System.Windows.Forms.TextBox txt_publicoAlvo;
+        private System.Windows.Forms.Label lbl_publicoAlvo;
+        private System.Windows.Forms.TextBox txt_regiao;
+        private System.Windows.Forms.Label lbl_regiao;
+        private System.Windows.Forms.Label lbl_obs;
+        private System.Windows.Forms.TextBox txt_obs;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ LDABDK/Form1.cs
@@ -0,0 +1,222 @@
+﻿using NixPDC;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using OpenQA.Selenium.Interactions;
+
+namespace botmap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void btn_iniciar_Click(object sender, EventArgs e)
+        {
+            //Entra na steam 
+            IWebDriver driver = SeleniumMetodos.criaDriver(Constantes.headless);
+            SeleniumMetodos.navegarPara(driver, "https://store.steampowered.com/login/?redir=&redir_ssl=1&snr=1_4_661__global-header");
+            
+
+            int i = 0;
+
+
+            do
+            {
+                FuncoesUteis.pausa(1000);//pede para fazer login e só continua o código depois do OK
+                MessageBox.Show("Faça login e DEPOIS que logar aperte o botão OK", "Instrução", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+
+                try
+                {
+                    SeleniumMetodos.clickPorClasse(driver, "login_btn");//clica no botão de login para verificar se ja saiu da tela de login (se não tem botão de login, não está na tela de login! GE-NI-AL kkk)
+                    
+                    
+                }
+
+                catch (Exception ex)
+                {
+
+                    if (ex is OpenQA.Selenium.NoSuchElementException)//caso não esteja na tela de login, a falta do botão vai causar um erro que vai somar +1 no contador e sair do laço
+                    {
+                        i++;
+                        
+
+                    }
+                    else
+                    {
+                        
+                    }
+
+
+
+                }
+
+            } while (i == 0);
+
+
+            
+
+
+            int tempo = 1000;
+
+            SeleniumMetodos.navegarPara(driver, "https://store.steampowered.com/explore/");//vai para a página da lista de descobrimento
+            FuncoesUteis.pausa(tempo);
+
+            try
+            {
+                //tentativas falhas de clicar no botão gigantesco no meio da página
+                //SeleniumMetodos.clickPorClasse(driver, "discovery_queue_overlay");
+                //SeleniumMetodos.clickPorXPATH(driver, "/html/body/div[1]/div[7]/div[5]/div[1]/div[3]/div/div[1]/div[2]/div[2]/span");
+                //SeleniumMetodos.clickPorId(driver, "refresh_queue_btn");
+
+
+                SeleniumMetodos.clickPorId(driver, "discovery_queue_start_link");//inicia a lista
+
+
+            }
+
+            catch (Exception ex2)
+            {
+                if (ex2 is OpenQA.Selenium.NoSuchElementException)
+                {
+                    
+                    //SeleniumMetodos.clickPorClasse(driver, "discovery_queue_overlay");
+                    //SeleniumMetodos.clickPorXPATH(driver, "/html/body/div[1]/div[7]/div[5]/div[1]/div[3]/div/div[1]/div[3]/a/div/div");
+                    SeleniumMetodos.clickPorId(driver, "discovery_queue_start_link"); //parte inutil do código, se ele não achar o botão ele vai avisar que a lista ja terminou de um jeito ou de outro
+
+                }
+                else
+                {
+
+                }
+            }
+            
+
+            FuncoesUteis.pausa(tempo);
+            
+
+            int j = 0;
+            do
+            {
+
+                try
+                {
+                    SeleniumMetodos.clickPorClasse(driver, "next_in_queue_content");// depois de iniciada vai passando a lista pra frente
+
+                }
+
+                catch (Exception ex2)
+                {
+                    if (ex2 is OpenQA.Selenium.NoSuchElementException)
+                    {
+                        //se não tem botão de next, a lista ja acabou (proteção contra listas que foram vistas e tem menos de 12 jogos)
+                        MessageBox.Show("Você ja viu sua lista inteira hoje!", "Prompt", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+                        j = j + 100;
+                    }
+                    else
+                    {
+
+                    }
+                }
+
+                j++;
+                FuncoesUteis.pausa(tempo);
+
+
+
+
+            } while (j  < 12);
+
+
+
+
+
+
+
+            if (j < 100)//para evitar que o mesmo aviso apareça duas vezes, essa execução é a execução se viu a lista toda, a de cima junto com o else abaixo é a execução de listas com menos de 12 jogos
+            {
+                
+                MessageBox.Show("Você ja viu sua lista inteira hoje!", "Instrução", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+                MessageBox.Show("Aperte OK para fechar o programa\nIsso vai fechar o navegador que abriu", "Instrução", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+                SeleniumMetodos.fecharDriver(driver);
+            }
+
+            else
+
+            {
+                
+                MessageBox.Show("Aperte OK para fechar o programa", "Instrução", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+                SeleniumMetodos.fecharDriver(driver);
+
+            }
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+    private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl_regiao_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_publicoAlvo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //feito por: Arthur Quintanilha, vulgo Paraíba BDK.
+
+    }
+
+    
+}
